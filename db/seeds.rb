e5ddf0565b907de204965f02d1c78cfd498ed583
nmackenzie91 <nmackenzie@live.co.uk>
2020-04-27 12:07:19 +0100

    manufacturer seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,38 @@ Product.delete_all()
 
 manufacturer1 = Manufacturer.new({
     "name" => "Anuj Woollen Mill"
-    "location"
-}
+    "address" => "Plot No-75, Industrial Area No-3, A.B. Road, Dewas - 450001, Madhya Pradesh, India"
+    "email" => "orders@anujwoollens.com"
+    "tel" => 917272258326
+    "product_speciality" => "Recycled Wool"
+    "shipping_times" => "2-4 weeks"
+})
+
+manufacturer1.save()
+
+manufacturer2 = Manufacturer.new({
+    "name" => "Northland Cashmere"
+    "address" => "Harmony Building, Jinchuan Science & Technology Area, Jin Er Road, Jinchuan Development Zone, Huhhot, China 010080 "
+    "email" => "tergel@NorthlandCashmere.com"
+    "tel" => 008615124760600
+    "product_speciality" => "Cashmere"
+    "shipping_times" => "4-6 weeks"
+})
+
+manufacturer2.save()
+
+
+
+manufacturer2 = Manufacturer.new({
+    "name" => "Lochcarron of Scotland"
+    "address" => "Waverely Mill, Rogers Road, Selkirk, Scotland, TD7 5DX "
+    "email" => "sales@lochcarron.com"
+    "tel" => 01750726123
+    "product_speciality" => "Lambswool"
+    "shipping_times" => "3-5 days"
+})
+
+manufacturer2.save()
+
 
 product1
