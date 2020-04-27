0e06b8dac9d6b640cd7f5907b84f5c14202a532e
nmackenzie91 <nmackenzie@live.co.uk>
2020-04-27 14:02:17 +0100

    resolved issues with tables, all now created and visible in Postico

--- db/products.sql
+++ db/products.sql
@@ -10,22 +10,22 @@ CREATE TABLE products
     quantity INT,
     buying_price INT,
     selling_price INT
-)
+);
 
 CREATE TABLE manufacturers
 (
     id SERIAL primary key,
     name VARCHAR(255) not null,
-    location VARCHAR(255) not null,
+    address VARCHAR(255) not null,
     email VARCHAR(255) not null,
     tel INT,
     product_speciality VARCHAR(255) not null,
     shipping_times VARCHAR(255) not null
-)
+);
 
 CREATE TABLE inventories
 (
     id SERIAL primary key,
-    product_id INT references product(id),
+    product_id INT references products(id),
     manufacturer_id INT references manufacturers(id)
-)
+);
--- models/inventory.rb
+++ models/inventory.rb
@@ -17,64 +17,64 @@ class Inventory
 #CRUD
 #Not sure if I will need all of the basic CRUD
 
-def save()
-    sql = "INSERT INTO inventories
-    (product_id, manufacturer_id)
-    VALUES
-    ($1, $2)
-    RETURNING id"
-    values = [@product_id, @manufacturer_id]
-    results = SqlRunner.run(sql, values)
-    @id = results.first()['id'].to_i
-end
+    def save()
+        sql = "INSERT INTO inventories
+        (product_id, manufacturer_id)
+        VALUES
+        ($1, $2)
+        RETURNING id"
+        values = [@product_id, @manufacturer_id]
+        results = SqlRunner.run(sql, values)
+        @id = results.first()['id'].to_i
+    end
 
 
-def update()
-    sql = "UPDATE inventories
-    SET
-    (product_id, manufacturer_id)
-    =
-    ($1, $2)
-    WHERE id = $3"    
-    (product_id, manufacturer_id)
-    SqlRunner.run(sql, values)
-end
-
-
-
-def delete()
-    sql = "DELETE FROM inventories WHERE id = $1"
-    values = [@id]
-    SqlRunner.run(sql, values)
-end
+    def update()
+        sql = "UPDATE inventories
+        SET
+        (product_id, manufacturer_id)
+        =
+        ($1, $2)
+        WHERE id = $3"    
+        (product_id, manufacturer_id)
+        SqlRunner.run(sql, values)
+    end
 
 
 
-
-def self.map_items(inventory_data)
-    return inventory_data.map { |inventory| Inventory.new(inventory) }
-end
-
-
-def self.all()
-    sql = "SELECT * FROM inventories"
-    inventory_data = SqlRunner.run(sql)
-    inventories = map_items(inventory_data)
-    return inventories
-end
+    def delete()
+        sql = "DELETE FROM inventories WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
 
 
-def self.find()
-    sql = "SELECT * FIND inventories WHERE id = $1"
-    values = [id]
-    results = SqlRunner.run(sql, values).first
-    return Inventory.new(results)
-end
 
-def self.delete_all()
-    sql = "DELETE FROM inventories"
-    SqlRunner.run(sql)
-end
+
+    def self.map_items(inventory_data)
+        return inventory_data.map { |inventory| Inventory.new(inventory) }
+    end
+
+
+    def self.all()
+        sql = "SELECT * FROM inventories"
+        inventory_data = SqlRunner.run(sql)
+        inventories = map_items(inventory_data)
+        return inventories
+    end
+
+
+    def self.find()
+        sql = "SELECT * FIND inventories WHERE id = $1"
+        values = [id]
+        results = SqlRunner.run(sql, values).first
+        return Inventory.new(results)
+    end
+
+    def self.delete_all()
+        sql = "DELETE FROM inventories"
+        SqlRunner.run(sql)
+    end
 
 
 
