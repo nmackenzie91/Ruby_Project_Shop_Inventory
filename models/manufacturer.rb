185acc1d50945282f1b34c54c97b8cb948f69440
nmackenzie91 <nmackenzie@live.co.uk>
2020-04-27 11:25:33 +0100

    basic CRUD for manufacturers added

--- models/manufacturer.rb
+++ models/manufacturer.rb
@@ -19,6 +19,66 @@ class Manufacturer
 
 
 #CRUD
+#Manufacturers page will need to be able to pull all necessary data from initialzie as well as updating their info, removing bankrupt companies and adding new ones
+
+    def save()
+        sql = "INSERT INTO manufacturers
+        (name, location, email, tel, product_speciality, shipping_times)
+        VALUES
+        ($1, $2, $3, $4, $5, $6)
+        RETURNING id"
+        values = [@name, @location, @email, @tel, @product_speciality, @shipping_times]
+        results = SqlRunner.run(sql, values)
+        @id = results.first()['id'].to_i
+    end
+
+
+    def update()
+        sql = "UPDATE manufacturers
+        SET
+        (name, location, email, tel, product_speciality, shipping_times)
+        =
+        ($1, $2, $3, $4, $5, $6)
+        WHERE id = $7"    
+        values = [@name, @location, @email, @tel, @product_speciality, @shipping_times]
+        SqlRunner.run(sql, values)
+    end
+
+
+
+    def delete()
+        sql "DELETE FROM manufacturers WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
+
+
+
+    def self.map_items(manufacturer_data)
+        return manufacturer_data.map { |manufacturer| Manufacturer.new(manufacturer) }
+    end
+
+
+    def self.all()
+        sql = "SELECT * FROM manufacturers"
+        manufacturer_data = SqlRunner.run(sql)
+        manufacturers = map_items(manufacturer_data)
+        return manufacturers
+    end
+
+
+    def self.find()
+        sql = "SELECT * FIND manufacturers WHERE id = $1"
+        values = [id]
+        results = SqlRunner.run(sql, values).first
+        return Manufacturer.new(results)
+    end
+
+    def self.delete_all()
+        sql = "DELETE FROM manufacturers"
+        SqlRunner.run(sql)
+    end
 
 
 end
--- models/product.rb
+++ models/product.rb
@@ -19,6 +19,7 @@ class Product
 
 
 #CRUD
+#Products
 
 
 
