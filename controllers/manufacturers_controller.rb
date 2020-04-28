12333838ab48e6cc462eec378de97ffa4f927c9b
nmackenzie91 <nmackenzie@live.co.uk>
2020-04-28 22:31:51 +0100

    basic buttons added for add,edit and delete manufacturers

--- controllers/manufacturers_controller.rb
+++ controllers/manufacturers_controller.rb
@@ -10,7 +10,38 @@ get '/manufacturers' do
     erb ( :"manufacturers/index" )
   end
 
+  get '/manufacturers/new' do
+    @manufacturers = Manufacturer.all()
+    erb( :"manufacturers/new" )
+  end
 
+
+  get '/manufacturers/:id' do  
+    @manufacturer = Manufacturer.find(params['id'].to_i)
+    erb( :"manufacturers/show" )
+  end
+
+  post '/manufacturers' do 
+    @manufacturer = Manufacturer.new( params )
+    @manufacturer.save()
+    redirect to '/manufacturers'
+  end
+
+  get '/manufacturers/:id/edit' do 
+    @manufacturer = Manufacturer.find( params[:id] )
+    erb( :"manufacturers/edit" )
+  end
+  
+  post '/manufacturers/:id' do 
+    Manufacturer.new( params ).update
+    redirect to '/manufacturers'
+  end
+  
+  post '/manufacturers/:id/delete' do 
+    manufacturer = Manufacturer.find( params[:id] )
+    manufacturer.delete()
+    redirect to '/manufacturers'
+  end
 
 
 #ReSTful routes
