ae68d35e2a26fb9cdd7717e6ce1819f68fe5ad80
nmackenzie91 <nmackenzie@live.co.uk>
2020-04-28 12:22:49 +0100

    finally got the add product form working. Other basics added too but not fleshed out

--- controllers/products_controller.rb
+++ controllers/products_controller.rb
@@ -5,12 +5,47 @@ also_reload( '../models/*' )
 
 
 
-get '/products' do
+get '/products' do  #index
     @products = Product.all()
     erb ( :"products/index" )
   end
 
 
+  get '/products/new' do # new
+    @products = Product.all()
+    @manufacturers = Manufacturer.all()
+    erb( :"products/new" )
+  end
+
+
+  get '/products/:id' do    #show
+    @product = Product.find(params['id'].to_i)
+    erb( :"products/show" )
+  end
+
+  post '/products' do # create
+    @product = Product.new( params )
+    @product.save()
+    erb( :"products/create" )
+  end
+
+  #redirect to products? ^^
+
+#   get '/products/:id/edit' do # edit
+#     @product = Product.find( params[:id] )
+#     erb( :edit )
+#   end
+  
+#   post '/products/:id' do # update
+#     Product.new( params ).update
+#     redirect to '/products'
+#   end
+  
+#   post '/products/:id/delete' do # delete
+#     product = Product.find( params[:id] )
+#     product.delete()
+#     redirect to '/products'
+#   end
 
 
 #ReSTful routes
